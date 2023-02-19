95424a936a8ba50370791bb7f0c505e64d8f07b2
Ling Wang <2505660478@qq.com>
2023-02-19 14:34:00 +0800

    add tuple-to-union

--- type-challenges/00009-medium-deep-readonly/template.ts
+++ type-challenges/00009-medium-deep-readonly/template.ts
@@ -1,3 +1,5 @@
-type DeepReadonly<T> = T extends Function ? T : {
-  readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P];
-};
+type DeepReadonly<T> = T extends Function
+  ? T
+  : {
+      readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P];
+    };
