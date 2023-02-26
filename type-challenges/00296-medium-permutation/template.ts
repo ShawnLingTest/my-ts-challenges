be324de5804bb29758712b113fc538024b1326d1
Ling Wang <2505660478@qq.com>
2023-02-26 17:42:58 +0800

    298-lengthOfString

--- type-challenges/00296-medium-permutation/template.ts
+++ type-challenges/00296-medium-permutation/template.ts
@@ -1,15 +1,1 @@
 type Permutation<T> = [];
-
-const testPermutation = <T extends string>(args: T[]) => {
-  const result: any[] = [];
-
-  function permute (arr, memo) {
-
-  }
-
-  return result;
-};
-
-const result = testPermutation(["1", "2", "3"]);
-
-console.log(result);
